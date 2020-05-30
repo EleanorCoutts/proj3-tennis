a4cabc147280386d7d9760e27cadc9e45248c1b2
Eleanor Coutts <eleanor_coutts@hotmail.co.uk>
2020-05-30 12:29:13 +0100

    change initialisation of neural networks

--- models.py
+++ models.py
@@ -1,34 +1,50 @@
 import torch.nn as nn
 import torch
 import torch.nn.functional as F
+import numpy as np
+
+def hidden_init(layer):
+	fan_in = layer.weight.data.size()[0]
+	lim = 1. / np.sqrt(fan_in)
+	return (-lim, lim)
 
 class actor_network(nn.Module):
-    def __init__(self, state_size, hidden1_size, hidden2_size, action_size):
-        super(actor_network, self).__init__()
-        self.fc1 = nn.Linear(state_size,hidden1_size)
-        self.fc2 = nn.Linear(hidden1_size,hidden2_size)
-        self.fc3 = nn.Linear(hidden2_size,action_size)
-        
-    def forward(self,state):
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        x = F.tanh(self.fc3(x))
-        return(x)
+	def __init__(self, state_size, hidden1_size, hidden2_size, action_size):
+		super(actor_network, self).__init__()
+		self.fc1 = nn.Linear(state_size,hidden1_size)
+		self.fc2 = nn.Linear(hidden1_size,hidden2_size)
+		self.fc3 = nn.Linear(hidden2_size,action_size)
+		self.reset_parameters()
+	
+	def reset_parameters(self):
+		self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+		self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+		self.fc3.weight.data.uniform_(-3e-3,3e-3)
+		
+	def forward(self,state):
+		x = F.relu(self.fc1(state))
+		x = F.relu(self.fc2(x))
+		x = F.tanh(self.fc3(x))
+		return(x)
 
 class critic_network(nn.Module):
-    def __init__(self, observation_size, hidden1_size, hidden2_size, action_size, n_agents):
-        #assume every agent has same action size and observation size
-        super(critic_network, self).__init__()
-        self.fc1 = nn.Linear((observation_size+action_size)*n_agents,hidden1_size)
-        self.fc2 = nn.Linear(hidden1_size,hidden2_size)
-        self.fc3 = nn.Linear(hidden2_size,action_size)
-        
-    def forward(self,all_observations,all_actions):
-        
-        x = torch.cat((all_observations,all_actions),dim=1) 
-        #here the action has been included from the 1st layer
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return(x)
+	def __init__(self, state_size, hidden1_size, hidden2_size, action_size):
+		super(critic_network, self).__init__()
+		self.fc1 = nn.Linear(state_size+action_size,hidden1_size)
+		self.fc2 = nn.Linear(hidden1_size,hidden2_size)
+		self.fc3 = nn.Linear(hidden2_size,action_size)
+		self.reset_parameters()
+		
+	def reset_parameters(self):
+		self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+		self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+		self.fc3.weight.data.uniform_(-3e-3,3e-3)
+		
+	def forward(self,state,action):
+		x = torch.cat((state,action),dim=1) 
+		#here the action has been included from the 1st layer, unlike DDPG paper which adds it in at 2nd.
+		x = F.relu(self.fc1(x))
+		x = F.relu(self.fc2(x))
+		x = self.fc3(x)
+		return(x)
 		
